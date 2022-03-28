7b46e343b5a89bc0f435fb19338f90414b199f8a
willymwaura <wilsonmwaura697@gmail.com>
2022-03-28 15:14:48 +0300

    settings

--- todo/settings.py
+++ todo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
